a8f83fab33da40eaff5cf6621174b27654bb8760
Ryan Smith <ryan.l.smith4@gmail.com>
2018-10-19 00:42:09 -0700

    I am playing league

--- models/blog.js
+++ models/blog.js
@@ -1,7 +1,7 @@
 //models/blog.js
 //formally know as models/review.js
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/contractor_proj', {
+mongoose.connect(process.env.MONGODB_URI ||' mongodb://localhost/contractor_proj', {
     useNewUrlParser: true
 });
 const Schema = mongoose.Schema
